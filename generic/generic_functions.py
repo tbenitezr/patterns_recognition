070cb44749948a7f8ac1955cd65939b249603b5e
Tania <tania@localhost.localdomain>
2016-10-07 10:27:12 -0500

    plot 2d

--- generic/generic_functions.py
+++ generic/generic_functions.py
@@ -5,27 +5,56 @@ from tkFileDialog import askopenfilename
 import Image, ImageTk
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
-def create_class(c, flag_img):
-    n_classes = input("Ingrese el número de clases: ") 
+def plot_2d(classes, vector, n_cs):  
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    markers = [ 
+                '.', ',', 'o',"v", "^", "<", ">", "1",
+                "2", "3", "4", "8", "s", "p", "*", "h",
+                "H", "+", "x", "D", "d", "|", "_", "TICKLEFT",  
+                "TICKRIGHT", "TICKUP", "TICKDOWN", "CARETLEFT",
+                "CARETRIGHT", "CARETUP", "CARETDOWN"
+            ]
+     
+    plt.figure()
+    figures = list() 
+    cont = 0
+
+    for matrix in classes:
+        cont += 1
+        label_figure =  "Clase "+ str(cont)
+        figures.append(plt.scatter(matrix[0], matrix[1], color=colors[cont], marker=markers[cont], label=label_figure))
+    
+    figures.append(plt.scatter(vector[0], vector[1], color='b', marker='*', label="Vector"))
+    plt.title("Grafica " + str(n_cs) + " clases")
+    plt.xlabel("Eje x")
+    plt.ylabel("Eje y")
+    plt.legend(handles=figures, loc=4, fontsize=8)
+    plt.show()
+
+def create_class(c, flag_img, 
+    n_classes = input("Ingrese el número de clases: "),
     n_repr = input("Número de representantes: ")
+    ):
     centers = list()
     if flag_img == -1:
-        provide_img(centers, n_classes) 
+        provide_img(centers, n_classes)
+        print 'Centros '
+        print centers         
         dimention = centers[0].shape[1]
-        print centers[0]
-        print dimention
     else:
-        dimention = raw_input("Dimensión de los representantes: ")
+        dimention = input("Dimensión de los representantes: ")
     for i in range(n_classes):
         print 'Para la clase {} \n'.format(i+1)
         dispersal = input("Dispersión: ")
         if flag_img != -1:
             center = list(map(int,raw_input("Centro separado por comas: ").split(", "))) 
         else:
+            pass
             print 'y Centro: {}'.format(centers[i])
-        c.append(np.transpose(np.random.rand(dimention,n_repr) * dispersal) + np.transpose(centers[i]))
-    print c
+        c.append(np.random.rand(dimention,n_repr) * dispersal + np.transpose(centers[i]))
+
 
 def provide_img(centers, n_classes):
     root = Tk()
@@ -52,14 +81,11 @@ def provide_img(centers, n_classes):
         prix = im.load()
         canvas.create_image(0,0,image=img,anchor="nw")
         canvas.config(scrollregion=canvas.bbox(ALL))
+        
         def get_coords(event):
-            #print (event.x,event.y)}
-            print len(centers)
-            if len(centers) != n_classes :
-                print 'elementos centros {}'.format(len(centers))
-                centers.append(np.matrix(prix[event.x,event.y]))
-                print centers
-            else:
+            centers.append(np.matrix(prix[event.x,event.y]))            
+            numbers_centers = len(centers)
+            if  numbers_centers == n_classes :
                 root.destroy()
                 return 
         canvas.bind("<Button 1>",get_coords)
@@ -69,4 +95,6 @@ def provide_img(centers, n_classes):
 
 if __name__ == "__main__":
     c = list()
-    create_class(c, -1)
+    create_class(c, -1, 2, 5)
+    vector = [20,5,-17]
+    plot_2d(c, vector, 2)
